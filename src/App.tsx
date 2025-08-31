c249b95954b2764cc3df191524a25cabbbf08764
gizag889 <gizaguri0426@outlook.jp>
2025-08-31 20:09:36 +0900

    shadcn/ui のセットアップ

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "./components/ui/card";
+import { Textarea } from "./components/ui/textarea";
+ 
+function App() {
+  return (
+    <Card className="w-96">
+      <CardHeader>shadcn/uiの表示確認</CardHeader>
+      <CardContent>
+        <Textarea />
+      </CardContent>
+      <CardFooter>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
+}
+ 
+export default App;
